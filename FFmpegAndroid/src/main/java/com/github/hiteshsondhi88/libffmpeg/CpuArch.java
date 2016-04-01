e51e22e403cc1c73606d2e60626a4fb64cb8023a
Federico Ramundo <federico.ramundo@wolox.com.ar>
2016-04-01 12:06:18 -0300

    ffmpeg 2.8

--- FFmpegAndroid/src/main/java/com/github/hiteshsondhi88/libffmpeg/CpuArch.java
+++ FFmpegAndroid/src/main/java/com/github/hiteshsondhi88/libffmpeg/CpuArch.java
@@ -3,9 +3,9 @@ package com.github.hiteshsondhi88.libffmpeg;
 import android.text.TextUtils;
 
 enum CpuArch {
-    x86("1b3daf0402c38ec0019ec436d71a1389514711bd"),
-    ARMv7("e27cf3c432b121896fc8af2d147eff88d3074dd5"),
-    ARMv7_NEON("9463c40e898c53dcac59b8ba39cfd590e2f1b1bf"),
+    x86("8ae30e9f1f9b01f2d8b08f30184c0ecb35a5de0b"),
+    ARMv7("98ff9e9de3fe356d7be3824c2a99a43b8d5a52cf"),
+    ARMv7_NEON("58a2181ef367bc4fdd214d13af2f0b957ab60dbd"),
     NONE(null);
 
     private String sha1;
